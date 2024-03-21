1db753afb89bdcb830c4b8d76f8e19ad43e2011d
palloncino <antonio.guiotto.dev@gmail.com>
2024-03-21 16:26:57 +0100

    progress

--- WEB-7/main.js
+++ WEB-7/main.js
@@ -8,15 +8,14 @@ function setup() {
   let p5Canvas = createCanvas(windowWidth, windowHeight);
   p5Canvas.id("p5Canvas");
   spheres = [
-    { x: -100, y: -20, size: 60, currentSize: 60, targetSize: 60, color: "#000", text: "" },
-    { x: 100, y: 20, size: 60, currentSize: 60, targetSize: 60, color: "#F79B00", text: "" },
-
-    { x: 0, y: 0, size: 60, currentSize: 60, targetSize: 60, color: "#F79B00", text: "Art direction" },
-    { x: 0, y: 0, size: 60, currentSize: 60, targetSize: 60, color: "#F79B00", text: "Design" },
-    { x: 0, y: 0, size: 60, currentSize: 60, targetSize: 60, color: "#F79B00", text: "Visual Art" },
-    { x: 0, y: 0, size: 60, currentSize: 60, targetSize: 60, color: "#F79B00", text: "Web development" },
-    { x: 0, y: 0, size: 60, currentSize: 60, targetSize: 60, color: "#F79B00", text: "Data analysis" },
-    { x: 0, y: 0, size: 60, currentSize: 60, targetSize: 60, color: "#F79B00", text: "Social media" },
+    { x: -100, y: -20, size: 80, currentSize: 80, targetSize: 80, color: "#000", text: "" },
+    { x: 100, y: 20, size: 80, currentSize: 80, targetSize: 80, color: "#F79B00", text: "" },
+    { x: 0, y: 0, size:80, currentSize:80, targetSize:80, color: "#F79B00", text: "Art direction" },
+    { x: 0, y: 0, size:80, currentSize:80, targetSize:80, color: "#F79B00", text: "Design" },
+    { x: 0, y: 0, size:80, currentSize:80, targetSize:80, color: "#F79B00", text: "Visual Art" },
+    { x: 0, y: 0, size:80, currentSize:80, targetSize:80, color: "#F79B00", text: "Web development" },
+    { x: 0, y: 0, size:80, currentSize:80, targetSize:80, color: "#F79B00", text: "Data analysis" },
+    { x: 0, y: 0, size:80, currentSize:80, targetSize:80, color: "#F79B00", text: "Social media" },
   ];
   setTimeout(() => currentPhase = (currentPhase + 1) % phases.length, 500)
 }
@@ -72,7 +71,7 @@ function draw() {
 
     // After 500ms have passed since the timer started, begin scaling
     if (scaleTimerStarted && millis() - scaleTimer > 500) {
-      spheres[0].currentSize = lerp(spheres[0].currentSize, 80, 0.05);
+      spheres[0].currentSize = lerp(spheres[0].currentSize, 120, 0.05);
     }
   }
 
@@ -80,22 +79,22 @@ function draw() {
     visibleSpheres = spheres.slice(2, 8);
 
     spheres[2].x = lerp(spheres[2].x, -500, 0.05);
-    spheres[2].y = lerp(spheres[2].y, 0, 0.05);
+    spheres[2].y = lerp(spheres[2].y, -150, 0.05);
 
     spheres[3].x = lerp(spheres[3].x, -300, 0.05);
-    spheres[3].y = lerp(spheres[3].y, 0, 0.05);
+    spheres[3].y = lerp(spheres[3].y, -250, 0.05);
 
     spheres[4].x = lerp(spheres[4].x, -100, 0.05);
-    spheres[4].y = lerp(spheres[4].y, 0, 0.05);
+    spheres[4].y = lerp(spheres[4].y, -300, 0.05);
 
     spheres[5].x = lerp(spheres[5].x, 100, 0.05);
-    spheres[5].y = lerp(spheres[5].y, 0, 0.05);
+    spheres[5].y = lerp(spheres[5].y, -300, 0.05);
 
     spheres[6].x = lerp(spheres[6].x, 300, 0.05);
-    spheres[6].y = lerp(spheres[6].y, 0, 0.05);
+    spheres[6].y = lerp(spheres[6].y, -250, 0.05);
 
     spheres[7].x = lerp(spheres[7].x, 500, 0.05);
-    spheres[7].y = lerp(spheres[7].y, 0, 0.05);
+    spheres[7].y = lerp(spheres[7].y, -150, 0.05);
   }
 
   if (phases[currentPhase] === "REDIRECT") {
